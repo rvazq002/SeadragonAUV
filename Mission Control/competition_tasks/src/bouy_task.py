89150f1fd9377c85dcf89af6a2a6b8aa19c57a72
juancvargas <juanceja004@gmail.com>
2019-06-10 21:43:36 -0700

    Adding state machine folder

--- Mission Control/competition_tasks/src/bouy_task.py
+++ Mission Control/competition_tasks/src/bouy_task.py
@@ -35,7 +35,7 @@ from std_msgs.msg import Bool, Float64, Int16
 # Define State Init
 class init(smach.State):
 	def __init__(self):
-		smach.State.__init__(self, outcomes=['start','wait'])
+		smach.State.__init__(self, outcomes=['start','track1','reset1'])
 		
 		'''	 
 			Publishers, Subscribers & Local variables go here
@@ -52,19 +52,24 @@ class init(smach.State):
 
 		# Check if condition for transitioning to next state was met here.
 		self.timer += 1
-		if self.timer > 20000:
+		if self.reset == True:
+            return 'reset' 
+        else self.timer > 20000:
 			self.timer = 0
 			return 'start'
 		else:
-			return 'wait'
+			return 'track1'
 
-# Define State A
-class A(smach.State):
+# Define State Track1
+# Purpose: The state tracks the location of the first bouy 
+# which contains the image of the vampire it is the single flat 
+# bouy. 
+class Track1(smach.State):
 	def __init__(self):
-		smach.State.__init__(self, outcomes=['timeup','nottime','reset'])
+		smach.State.__init__(self, outcomes=['track1','touch1', 'reset'])
 		
 		# Publishers, Subscribers
-		self.reset_subscriber = rospy.Subscriber('/reset', Bool, self.reset_callback) 
+		self.reset_subscriber = rospy.Subscriber('/close', Bool, self.reset_callback) 
 		
 		# Local Variables
 		self.timer = 0
@@ -80,14 +85,19 @@ class A(smach.State):
 			return 'reset'
 		elif self.timer > 20000:
 			self.timer = 0
-			return 'timeup'
+			return 'track1'
 		else:
-			return 'nottime'			
+			return 'touch1'	
 
-# Define State B
-class B(smach.State):
+# Define State Touch1
+# Purpose: The state uses a preset timer to get the 
+# sub to touch the flat bouy which contains the image 
+# of a vampire.
+# NOTE: The timer was set based on the distance the 
+# sub has to travel to touch the bouy 
+class Touch1(smach.State):
         def __init__(self):
-                smach.State.__init__(self, outcomes=['tracking','task_complete','reset'])
+                smach.State.__init__(self, outcomes=['touch1','turn','reset'])
 
                 # Publishers, Subscribers
                 self.reset_subscriber = rospy.Subscriber('/reset', Bool, self.reset_callback)
@@ -106,14 +116,156 @@ class B(smach.State):
                         return 'reset'
                 elif self.timer > 20000:
                         self.timer = 0
-                        return 'task_complete'
+                        return 'touch1'
                 else:
-                        return 'tracking'
+                        return 'turn'	
+
+# Define State Turn
+# Purpose: The state resets the sub to face the the second 
+# bouy which rotates at a certain speed. 
+class Turn(smach.State):
+        def __init__(self):
+                smach.State.__init__(self, outcomes=['turn','wait2','reset'])
+
+                # Publishers, Subscribers
+                self.reset_subscriber = rospy.Subscriber('/reset', Bool, self.reset_callback)
+
+                # Local Variables
+                self.timer = 0
+                self.reset = False
+
+        def reset_callback(self,msg):
+                self.reset = msg.data
+
+        def execute(self, userdata):
+
+                self.timer += 1
+                if self.reset == True:
+                        return 'reset'
+                elif self.timer > 20000:
+                        self.timer = 0
+                        return 'turn'
+                else:
+                        return 'wait2'
+
+# Define State Wait2
+# Purpose: The state waits until the computer vision finds the 
+# predefined image from the 3 possible choosen images that are located 
+# on the rotating bouy.  
+class Wait2(smach.State):
+        def __init__(self):
+                smach.State.__init__(self, outcomes=['wait2','touch2','reset'])
+
+                # Publishers, Subscribers
+                self.reset_subscriber = rospy.Subscriber('/reset', Bool, self.reset_callback)
+
+                # Local Variables
+                self.timer = 0
+                self.reset = False
+
+        def reset_callback(self,msg):
+                self.reset = msg.data
+
+        def execute(self, userdata):
+
+                self.timer += 1
+                if self.reset == True:
+                        return 'reset'
+                elif self.timer > 20000:
+                        self.timer = 0
+                        return 'wait2'
+                else:
+                        return 'touch2'
+
+# Define State Touch2
+# Purpose: The state uses a timer to move a certain distance to touch the bouy. 
+# The time was set depending on the distance of the sub from the bouy. 
+class Touch2(smach.State):
+        def __init__(self):
+                smach.State.__init__(self, outcomes=['touch2','setyaw','reset'])
+
+                # Publishers, Subscribers
+                self.reset_subscriber = rospy.Subscriber('/reset', Bool, self.reset_callback)
+
+                # Local Variables
+                self.timer = 0
+                self.reset = False
+
+        def reset_callback(self,msg):
+                self.reset = msg.data
+
+        def execute(self, userdata):
+
+                self.timer += 1
+                if self.reset == True:
+                        return 'reset'
+                elif self.timer > 20000:
+                        self.timer = 0
+                        return 'touch2'
+                else:
+                        return 'setyaw'
+
+# Define State SetYaw
+# The state prepares the sub for the next competiton task.
+# The sub has to be orientated to point in the direction of the 
+# "stake through the heart" task. 
+class SetYaw(smach.State): 
+        def __init__(self):
+                smach.State.__init__(self, outcomes=['setyaw','done','reset'])
+
+                # Publishers, Subscribers
+                self.reset_subscriber = rospy.Subscriber('/reset', Bool, self.reset_callback)
+
+                # Local Variables
+                self.timer = 0
+                self.reset = False
+
+        def reset_callback(self,msg):
+                self.reset = msg.data
+
+        def execute(self, userdata):
+
+                self.timer += 1
+                if self.reset == True:
+                        return 'reset'
+                elif self.timer > 20000:
+                        self.timer = 0
+                        return 'setyaw'
+                else:
+                        return 'done'
+
+# Define State Done
+# Purpose: The state sets sends a signal to the main state machine letting it know that the 
+# the task is complete. 
+class Done(smach.State):
+        def __init__(self):
+                smach.State.__init__(self, outcomes=['done','init','reset'])
+
+                # Publishers, Subscribers
+                self.reset_subscriber = rospy.Subscriber('/reset', Bool, self.reset_callback)
+
+                # Local Variables
+                self.timer = 0
+                self.reset = False
+
+        def reset_callback(self,msg):
+                self.reset = msg.data
+
+        def execute(self, userdata):
+
+                self.timer += 1
+                if self.reset == True:
+                        return 'reset'
+                elif self.timer > 20000:
+                        self.timer = 0
+                        return 'done'
+                else:
+                        return 'init'
 
 # Define State Reset
 class reset(smach.State):
 	def __init__(self):
-		smach.State.__init__(self, outcomes=['restart','wait'])
+		smach.State.__init__(self, outcomes=['restart','init'])
 		
 		self.timer = 0
 	
@@ -122,7 +274,7 @@ class reset(smach.State):
 		if self.timer > 20000:
 			return 'restart'
 		else: 
-			return 'wait'
+			return 'init'
 
 ##-------------------------- END STATE DEFINITIONS ------------------------------------##
 
